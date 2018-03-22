51edcdfdc31659bd121851972fca54ac5e10d0b7
Andrew Ray <notaghost227@gmail.com>
2018-03-21 23:02:25 -0400

    Create helper methods to translate Flag to Role
    The previous versions used a set of if/else clauses to translate a string into the proper Enum. However, this code is more robust, and isolates the responsibility for generating a Role enum to the Role.java file.

--- src/Role.java
+++ src/Role.java
@@ -5,7 +5,31 @@
  * @author andrewray
  */
 public enum Role {
-    FORWARDER,
-    SENDER,
-    RECEIVER
+    FORWARDER("F", "Forwarder"),
+    SENDER("S", "Sender"),
+    RECEIVER("R", "Receiver");
+	
+	private String argName;
+	private String name;
+	
+	Role(String shorthand, String longhand) {
+		argName = shorthand;
+		name = longhand;
+	}
+	
+	public String toString() {
+		return name;
+	}
+	
+	public static Role fromString(String shortName) {
+		switch (shortName) {
+		case "f":
+		case "F": return Role.FORWARDER;
+		case "s":
+		case "S": return Role.SENDER;
+		case "r":
+		case "R": return Role.RECEIVER;
+		default:  throw new IllegalArgumentException(shortName + " is not a valid Role");
+		}
+	}
 }
