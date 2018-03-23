272f0e207c390fb9c7a099e21d4917d272b3be5b
Andrew Ray <notaghost227@gmail.com>
2018-03-22 20:02:31 -0400

    Refactor Argument Parsing
    The argument parsing functionality was made more robust, checking for more failure modes than previously. The failure modes now return more descriptive error messages back to the user.

--- src/App.java
+++ src/App.java
@@ -1,27 +1,92 @@
+import java.io.PrintStream;
 import java.lang.Thread;
+import java.util.function.Function;
 
 /**
- * This class instantiates the whole Ringo application.
- * Given the correct number and type of command-line arguments,
- * this class starts a Ringo node as a child Thread
+ * This class instantiates the whole Ringo application. Given the correct number
+ * and type of command-line arguments, this class starts a Ringo node as a child
+ * Thread
  * 
  * @author sainaidu
  */
-public class App {	
-    public static void main(String[] args) {
-    		Role role;
-    		if (args[0] == "S") {
-    			role = Role.SENDER;
-    		} else if (args[0] == "R") {
-    			role = Role.RECEIVER;
-    		} else {
-    			role = Role.FORWARDER;
-    		}
-    		
-    		Thread ringoThread = new Thread(new Ringo(role, Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4])));
-    		ringoThread.start();
+public class App {
+	public static final int NUM_ARGS = 5;
+	public static final int MAX_PORT = 65535;
+
+	private static Role role;
+	private static int port;
+	private static String pocHost;
+	private static int pocPort;
+	private static int n;
+
+	public static void main(String[] args) {
+		try {
+			parseArguments(args);
+		} catch (IllegalArgumentException e) {
+			printHelp(System.err);
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+
+		Thread ringoThread = new Thread(new Ringo(role, port, pocHost, pocPort, n));
+		ringoThread.start();
     		while (ringoThread.isAlive()) {}
-    		
-    		System.exit(0);
-    }
+
+		System.exit(0);
+	}
+
+	private static void parseArguments(String[] args) throws IllegalArgumentException {
+		if (args.length < NUM_ARGS) {
+			printHelp(System.err);
+			throw new IllegalArgumentException("At least four arguments must be given");
+		}
+
+		ArgumentChecker<String, Integer> intchecker = new ArgumentChecker<>((String arg) -> Integer.parseInt(arg));
+		ArgumentChecker<String, String> hostchecker = new ArgumentChecker<>((String arg) -> (arg.equals("0") ? null : arg));
+
+		role = Role.fromString(args[0]);
+		port = intchecker.check(args[1], "Given Local Port isn't valid", 
+			(Integer i) -> (i > 0) && (i <= MAX_PORT)
+		);
+		pocHost = hostchecker.check(args[2], "Given POC Host isn't valid", (String s) -> new Boolean(true));
+		pocPort = intchecker.check(args[3], "Provided POC Port isn't valid", 
+			(Integer i) -> 
+				((pocHost == null && i == 0) || (pocHost != null && i > 0))
+				&& (i >= 0)
+				&& (i <= MAX_PORT)
+		);
+		n = intchecker.check(args[4], "Given N isn't valid", (Integer i) -> i > 0);
+	}
+
+	private static void printHelp(PrintStream stream) {
+		stream.println("Runtime Arguments:");
+		stream.println("java Ringo <Role> <Local Port> <PoC Hostname> <PoC Port> <N>");
+		stream.println("- Role: Sets this Ringo to be either a (S)ender, (R)eceiver, or a (F)orwarder");
+		stream.println("- PoC Hostname: The Hostname of the Point of Contact. Set to 0 if no PoC");
+		stream.println("- PoC Port: Port to use while contacting the PoC. Set to 0 if no PoC");
+		stream.println("- N: Number of Ringos in the mesh");
+		stream.println("");
+	}
+}
+
+class ArgumentChecker<T, R> {
+	
+	Function<T, R> mutator;
+	
+	public ArgumentChecker(Function<T, R> mutator) {
+		this.mutator = mutator;
+	}
+	
+	public R check(T arg, String errmsg, Function<R, Boolean> checker) throws IllegalArgumentException {
+		try {
+			R converted = mutator.apply(arg);
+			boolean sane = checker.apply(converted);
+			if (!sane)
+				throw new IllegalArgumentException();
+			return converted;
+		} catch (Throwable e) {
+			String extraInfo = (e.getMessage() != null) ? ": " + e.getMessage() : "";
+			throw new IllegalArgumentException(errmsg + extraInfo);
+		}
+	}
 }
