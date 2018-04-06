65a9a9f8c129b579633cc2164c6bf27978225bdb
Andrew Ray <aray40@gatech.edu>
2018-04-05 21:37:22 -0400

    Move the ArgumentChecker to another file

--- src/App.java
+++ src/App.java
@@ -84,24 +84,3 @@ public class App {
 	}
 }
 
-class ArgumentChecker<T, R> {
-	
-	Function<T, R> mutator;
-	
-	public ArgumentChecker(Function<T, R> mutator) {
-		this.mutator = mutator;
-	}
-	
-	public R check(T arg, String errmsg, Function<R, Boolean> checker) throws IllegalArgumentException {
-		try {
-			R converted = mutator.apply(arg);
-			boolean sane = checker.apply(converted);
-			if (!sane)
-				throw new IllegalArgumentException();
-			return converted;
-		} catch (Throwable e) {
-			String extraInfo = (e.getMessage() != null) ? ": " + e.getMessage() : "";
-			throw new IllegalArgumentException(errmsg + extraInfo);
-		}
-	}
-}
